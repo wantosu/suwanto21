9818580035d207a1edb4af95aa3e88a36e0ee720
wantosu <80557679+wantosu@users.noreply.github.com>
2021-03-16 20:06:17 +0700

    Add files via upload

---
+++ promoalaska1.js
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>promo alaska</title>
+</head>
+<body>
+<script>
+document.body.innerHTML = "<div class='section' id='license' name='License'><div class='widget HTML' data-version='2' id='HTML99'>"
+<div class='license-code'>MaNcMax6LWFcMGF7MaF4LWJ9NmIez70bzD1c</div></div>
+</script>
+</body>
+</html>
